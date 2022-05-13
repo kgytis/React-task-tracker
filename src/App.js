a2894df5b1ebafa6daef3653349a640269161b2e
kgytis <kaziukon.gytis@gmail.com>
2022-05-13 12:50:15 +0300

    Preparation for routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-
+import Footer from './components/Footer';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -90,6 +90,7 @@ function App() {
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? < Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show'}
+      <Footer />
     </div>
   );
 }
